4ec3776a91572ce78f8e46401384e3f6c1a3b340
nurnewas <nurnewasjr@gmail.com>
2025-03-22 09:03:50 -0600

    Set Time Out --(wait for some milli sec

--- JS/Js.js
+++ JS/Js.js
@@ -1,1 +1,9 @@
-console.log('JS/Js.js');
+let a = 5;
+function add(num1, num2) {
+    result = num1 + num2 + a;
+    return  result;
+}
+const sum = add(3, 4); // 12
+console.log(sum);
+
+// console.log(result); // this is a global variable
---
+++ JS/asic.js
@@ -0,0 +1,14 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+console.log(1);
+console.log(2);
+// setTimeout(DoSomething);
+setTimeout(() => {
+  console.log("i am laggy!");
+},4000);
+console.log(4);
+console.log(6);
+function DoSomething() {
+  console.log(3);
+}
