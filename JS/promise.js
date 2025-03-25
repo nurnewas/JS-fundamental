83edd1a9c93b79eff0f63e1330860ccaca98d579
nurnewas <nurnewasjr@gmail.com>
2025-03-25 13:26:11 -0600

    async and await --function and aro function

--- JS/promise.js
+++ JS/promise.js
@@ -8,3 +8,16 @@ const myLoader = () => {
     }
   });
 };
+
+async function loadData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.formData();
+  console.log(data);
+}
+loadData();
+
+const taskLoader = async () => {
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+}
